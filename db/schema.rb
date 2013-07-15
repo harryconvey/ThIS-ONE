9c4676150731e70aae2af3921b28d7afdca4ef7e
Harry Convey <harry.convey@gmail.com>
2013-07-15 17:14:51 +0100

    adding friends

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130518171913) do
+ActiveRecord::Schema.define(:version => 20130715160853) do
 
   create_table "statuses", :force => true do |t|
     t.text     "content"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(:version => 20130518171913) do
   end
 
   add_index "statuses", ["user_id"], :name => "index_statuses_on_user_id"
+
+  create_table "user_friendships", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name"

01dfb34debe1dd085e90e69d46093bb26a15319d
Harry Convey <harry.convey@gmail.com>
2013-07-15 17:23:08 +0100

    added lots of tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:user_friendships)
+  should have_many(:friends)
+  should have_many(:pending_user_friendships)
+  should have_many(:pending_friends)
+  should have_many(:requested_user_friendships)
+  should have_many(:requested_friends)
+  should have_many(:blocked_user_friendships)
+  should have_many(:blocked_friends)
+
   test "a user should enter a first name" do
     user = User.new
     assert !user.save
@@ -29,8 +38,8 @@ class UserTest < ActiveSupport::TestCase
 
   test "a user should have a profile name without spaces" do
     user = User.new(first_name: 'Jason', last_name: 'Seifer', email: 'jason2@teamtreehouse.com')
-    user.profile_name = 'Mike The Frog'
     user.password = user.password_confirmation = 'asdfasdf'
+    user.profile_name = "My Profile With Spaces"
 
     assert !user.save
     assert !user.errors[:profile_name].empty?
@@ -43,5 +52,31 @@ class UserTest < ActiveSupport::TestCase
 
     user.profile_name = 'jasonseifer_1'
     assert user.valid?
+  end
+
+  test "that no error is raised when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:jason).friends
+    end
+  end
+
+  test "that creating friendships on a user works" do
+    users(:jason).pending_friends << users(:mike)
+    users(:jason).pending_friends.reload
+    assert users(:jason).pending_friends.include?(users(:mike))
+  end
+
+  test "that calling to_param on a user returns the profile_name" do
+    assert_equal "jasonseifer", users(:jason).to_param
+  end
+
+  context "#has_blocked?" do
+    should "return true if a user has blocked another user" do
+      assert users(:jason).has_blocked?(users(:blocked_friend))
+    end
+
+    should "return false if a user has not blocked another user" do
+      assert !users(:jason).has_blocked?(users(:jim))
+    end
   end
 end
